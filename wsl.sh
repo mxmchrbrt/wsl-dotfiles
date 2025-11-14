736bfe1b8fd1de3756ddda4776dad38443974e1c
user <user@Desktop.localdomain>
2025-11-13 22:27:52 -0800

    a

---
+++ wsl.sh
@@ -0,0 +1,10 @@
+sudo apt install curl gpg
+source /etc/os-release
+echo "deb http://download.opensuse.org/repositories/shells:/fish/Debian_$VERSION_ID/ /" | sudo tee /etc/apt/sources.list.d/fish.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish/Debian_$VERSION_ID/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish.gpg > /dev/null
+sudo apt update 
+sudo apt install fish
+sudo apt update && sudo apt upgrade && sudo apt install wget tmux vim python3 gcc ranger git
+sudo echo /bin/fish | sudo tee -a /etc/shells
+chsh -s /bin/fish
+exit
